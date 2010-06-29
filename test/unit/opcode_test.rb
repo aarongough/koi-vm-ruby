359ebb7ca8d017637fd2824d4c99b0dae4c739a5
Aaron Gough <aaron@aarongough.com>
2010-06-28 22:36:04 -0400

    added failing tests for SWAP, JUMP, JUMP_IF and JUMP_UNLESS

--- test/unit/opcode_test.rb
+++ test/unit/opcode_test.rb
@@ -61,6 +61,26 @@ class OpcodeTest < Test::Unit::TestCase
     end
   end
   
+  test "SWAP REGA and REGB" do
+    state = VM.debug_run [
+      SET, REGA, "Hello",
+      SET, REGB, "world!",
+      SWAP, REGA, REGB
+    ]
+    assert_equal "world!", state[:registers][REGA]
+    assert_equal "Hello", state[:registers][REGB]
+    assert_nil state[:registers][DEBG]
+  end
+  
+  test "SWAP without operands should raise RuntimeError" do
+    assert_raises RuntimeError do
+      VM.debug_run [
+        SET, REGA, "Test",
+        SWAP
+      ]
+    end
+  end
+  
   test "should PUSH REGA onto stack" do
     state = VM.debug_run [
       SET,  REGA, "test",
@@ -216,10 +236,10 @@ class OpcodeTest < Test::Unit::TestCase
     end
   end
   
-  test "GOTO should set instruction_pointer" do
+  test "JUMP should set instruction_pointer to value in REGA" do
     state = VM.debug_run [
       SET, REGA, 9,
-      GOTO, REGA,
+      JUMP, REGA,
       SET, REGA, 1,
       NO_OP,
       NO_OP
@@ -230,10 +250,100 @@ class OpcodeTest < Test::Unit::TestCase
     assert_equal 0, state[:stack].size
   end
   
-  test "GOTO without operand should raise RuntimeError" do
+  test "JUMP should set instruction_pointer to value in REGB" do
+    state = VM.debug_run [
+      SET, REGB, 9,
+      JUMP, REGB,
+      SET, REGB, 1,
+      NO_OP,
+      NO_OP
+    ]
+    assert_equal 9, state[:registers][REGB]
+    assert_nil state[:registers][REGA]
+    assert_nil state[:registers][DEBG]
+    assert_equal 0, state[:stack].size
+  end
+  
+  test "JUMP without operand should raise RuntimeError" do
     assert_raises RuntimeError do
       VM.debug_run [
-        GOTO
+        JUMP
+      ]
+    end
+  end
+  
+  test "JUMP_IF should set instruction_pointer to value in REGA" do
+    state = VM.debug_run [
+      SET, REGA, 13,
+      SET, REGB, 1,
+      JUMP_IF, REGA, REGB,
+      SET, REGA, 1,
+      NO_OP,
+      NO_OP
+    ]
+    assert_equal 13, state[:registers][REGA]
+    assert_equal 1, state[:registers][REGB]
+    assert_nil state[:registers][DEBG]
+    assert_equal 0, state[:stack].size
+  end
+  
+  test "JUMP_IF should not set instruction_pointer to value in REGA" do
+    state = VM.debug_run [
+      SET, REGA, 13,
+      SET, REGB, 0,
+      JUMP_IF, REGA, REGB,
+      SET, REGA, 1,
+      NO_OP,
+      NO_OP
+    ]
+    assert_equal 1, state[:registers][REGA]
+    assert_equal 0, state[:registers][REGB]
+    assert_nil state[:registers][DEBG]
+    assert_equal 0, state[:stack].size
+  end
+  
+  test "JUMP_IF without operand should raise RuntimeError" do
+    assert_raises RuntimeError do
+      VM.debug_run [
+        JUMP_IF
+      ]
+    end
+  end
+  
+  test "JUMP_UNLESS should set instruction_pointer to value in REGA" do
+    state = VM.debug_run [
+      SET, REGA, 13,
+      SET, REGB, 0,
+      JUMP_UNLESS, REGA, REGB,
+      SET, REGA, 1,
+      NO_OP,
+      NO_OP
+    ]
+    assert_equal 13, state[:registers][REGA]
+    assert_equal 0, state[:registers][REGB]
+    assert_nil state[:registers][DEBG]
+    assert_equal 0, state[:stack].size
+  end
+  
+  test "JUMP_UNLESS should not set instruction_pointer to value in REGA" do
+    state = VM.debug_run [
+      SET, REGA, 13,
+      SET, REGB, 1,
+      JUMP_UNLESS, REGA, REGB,
+      SET, REGA, 1,
+      NO_OP,
+      NO_OP
+    ]
+    assert_equal 1, state[:registers][REGA]
+    assert_equal 1, state[:registers][REGB]
+    assert_nil state[:registers][DEBG]
+    assert_equal 0, state[:stack].size
+  end
+  
+  test "JUMP_UNLESS without operand should raise RuntimeError" do
+    assert_raises RuntimeError do
+      VM.debug_run [
+        JUMP_UNLESS
       ]
     end
   end
