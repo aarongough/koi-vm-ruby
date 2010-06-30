55720bb725499d3fcbd591f37790e849e5e89ca1
Aaron Gough <aaron@aarongough.com>
2010-06-30 16:04:33 -0400

    Added SUBTRACT operand

--- lib/koi/opcodes/subtract.rb
+++ lib/koi/opcodes/subtract.rb
@@ -1,5 +1,14 @@
 module Koi
   class VM
     
+    # SUBTRACT
+    def opcode9
+      raise StackError, "SUBTRACT: Not enough items on stack at offset: #{@state[@state_identifier][:instruction_pointer]}. Stack size: #{@state[@state_identifier][:stack].size}" if(@state[@state_identifier][:stack].size < 2)
+      right_operand = @state[@state_identifier][:stack].pop
+      left_operand = @state[@state_identifier][:stack].pop
+      @state[@state_identifier][:stack].push(left_operand - right_operand)
+      @state[@state_identifier][:instruction_pointer] += 1
+    end
+    
   end
 end
