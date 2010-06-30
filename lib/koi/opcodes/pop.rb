b23952ee58d334c04e08bc895d1c4684cdd1678c
Aaron Gough <aaron@aarongough.com>
2010-06-30 15:10:50 -0400

    Added POP opcode

--- lib/koi/opcodes/pop.rb
+++ lib/koi/opcodes/pop.rb
@@ -1,5 +1,11 @@
 module Koi
   class VM
     
+    # POP
+    def opcode2
+      raise StackError, "POP: Nothing on stack at offset: #{@state[@state_identifier][:instruction_pointer]}" if(@state[@state_identifier][:stack].pop.nil?)
+      @state[@state_identifier][:instruction_pointer] += 1
+    end
+    
   end
 end
