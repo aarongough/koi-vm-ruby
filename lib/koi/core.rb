97ef27301da56cb70572e5da8bc922de801f5d56
Aaron Gough <aaron@aarongough.com>
2010-06-30 16:13:02 -0400

    Added EXIT opcode

--- lib/koi/core.rb
+++ lib/koi/core.rb
@@ -19,7 +19,7 @@ module Koi
       while (@state[@state_identifier][:instruction_pointer] < @opcodes.size)
         opcode_name = "opcode" + @opcodes[@state[@state_identifier][:instruction_pointer]].to_s
         if(methods.include?(opcode_name.to_sym))
-          send(opcode_name)
+          break if(send(opcode_name))
         else
           raise RuntimeError, "Invalid opcode: #{@opcodes[@state[@state_identifier][:instruction_pointer]]}"
         end
--- lib/koi/opcodes/exit.rb
+++ lib/koi/opcodes/exit.rb
@@ -1,5 +1,11 @@
 module Koi
   class VM
     
+    # EXIT
+    def opcode12
+      @state[@state_identifier][:instruction_pointer] += 1
+      return true
+    end    
+    
   end
 end
