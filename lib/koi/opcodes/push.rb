50eaf3265c71946325a4cdfb85930abe8b8d185c
Aaron Gough <aaron@aarongough.com>
2010-06-30 16:14:39 -0400

    Updated PUSH to return OperandError when not supplied with an operand

--- lib/koi/opcodes/push.rb
+++ lib/koi/opcodes/push.rb
@@ -3,7 +3,7 @@ module Koi
     
     # PUSH
     def opcode1
-      raise RuntimeError, "Operand not supplied to PUSH at offset: #{@state[@state_identifier][:instruction_pointer]}" if(@opcodes[@state[@state_identifier][:instruction_pointer] + 1].nil?)
+      raise OperandError, "Operand not supplied to PUSH at offset: #{@state[@state_identifier][:instruction_pointer]}" if(@opcodes[@state[@state_identifier][:instruction_pointer] + 1].nil?)
       @state[@state_identifier][:stack].push @opcodes[@state[@state_identifier][:instruction_pointer] + 1]
       @state[@state_identifier][:instruction_pointer] += 2
     end
