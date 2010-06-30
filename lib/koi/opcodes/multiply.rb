97efd7cb8ff790a5179e20b4897006722cc86d7c
Aaron Gough <aaron@aarongough.com>
2010-06-30 16:06:00 -0400

    Added MULTIPLY opcode

--- lib/koi/opcodes/multiply.rb
+++ lib/koi/opcodes/multiply.rb
@@ -1,5 +1,12 @@
 module Koi
   class VM
     
+    # MULTIPLY
+    def opcode10
+      raise StackError, "MULTIPLY: Not enough items on stack at offset: #{@state[@state_identifier][:instruction_pointer]}. Stack size: #{@state[@state_identifier][:stack].size}" if(@state[@state_identifier][:stack].size < 2)
+      @state[@state_identifier][:stack].push(@state[@state_identifier][:stack].pop * @state[@state_identifier][:stack].pop)
+      @state[@state_identifier][:instruction_pointer] += 1
+    end
+    
   end
 end
