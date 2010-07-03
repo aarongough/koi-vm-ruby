094166063f7e5a188a32859780cbf51cd1384d7b
Aaron Gough <aaron@aarongough.com>
2010-07-03 16:23:07 -0400

    Fixed bug with NEW_FIBER that would cause it to switch to a fiber other than the newly created one

--- lib/koi/opcodes/new_fiber.rb
+++ lib/koi/opcodes/new_fiber.rb
@@ -5,7 +5,7 @@ module Koi
     def opcode23
       this_fiber[:instruction_pointer] += 1
       @state[:fibers].push Marshal.load(Marshal.dump(@state[:fibers][@state[:fiber_id]]))
-      @state[:fiber_id] += 1
+      @state[:fiber_id] = @state[:fibers].size - 1
     end
     
   end
--- test/unit/opcodes/new_fiber_test.rb
+++ test/unit/opcodes/new_fiber_test.rb
@@ -16,6 +16,29 @@ class NewFiberTest < Test::Unit::TestCase
     assert_equal 1, state[:fiber_id]
   end
   
+  test "NEW_FIBER should copy fiber state to new fiber and switch execution to the new fiber (2 fibers)" do
+    vm = VM.new({
+      :fiber_id => 0,
+      :fibers => [{
+        :stack => [],
+        :locals => [],
+        :instruction_pointer => 0
+        },{
+        :stack => [],
+        :locals => [],
+        :instruction_pointer => 0
+      }]
+    })
+    state = vm.run [
+      NEW_FIBER
+    ]
+    assert_equal 3, state[:fibers].length
+    assert_equal 1, state[:fibers][0][:instruction_pointer]
+    assert_equal 0, state[:fibers][1][:instruction_pointer]
+    assert_equal 1, state[:fibers][2][:instruction_pointer]
+    assert_equal 2, state[:fiber_id]
+  end
+  
   test "code after NEW_FIBER should not affect state of old fiber" do
     vm = VM.new
     state = vm.run [
