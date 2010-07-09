0b49dd3a0852b03bfe8265a46f97fa408b652f54
Aaron Gough <aaron@aarongough.com>
2010-07-09 08:38:29 -0400

    Updated examples to use TO_STRING and FILE_PUT

--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -3,6 +3,9 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'koi.rb'
 include Koi
 
 VM.new.run [
+  PUSH, 1,
+  PUSH, "w",
+  OPEN_FILE,
   PUSH, "Hello World!\n",
-  PRINT
+  FILE_PUT
 ]
