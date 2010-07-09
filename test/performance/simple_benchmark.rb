35f35a5e91d9605bc4a1a2723fb0b3dc6d6403c0
Aaron Gough <aaron@aarongough.com>
2010-07-09 09:05:37 -0400

    Updated benchmarks to use newer opcodes

--- test/performance/simple_benchmark.rb
+++ test/performance/simple_benchmark.rb
@@ -9,6 +9,10 @@ class SimpleBenchmark < Test::Unit::TestCase
   
   vm = VM.new
   
+  def teardown
+    File.delete("test.html")
+  end
+  
   test "simple benchmark" do
     Benchmark.bmbm do |b|
       b.report("Ruby") do
@@ -28,16 +32,25 @@ class SimpleBenchmark < Test::Unit::TestCase
         silence do
           1000.times do
             vm.run [
-              PUSH, 11,
+              PUSH, "test.html",
+              PUSH, "w",
+              OPEN_FILE,
+              SET_GLOBAL, 1,
+              PUSH, 11,   
               DECREMENT,
               DUP,
-              PRINT,
+              GET_GLOBAL, 1,
+              SWAP,
+              TO_STRING,
+              FILE_PUT,
+              GET_GLOBAL, 1,
               PUSH, ", ",
-              PRINT,
+              FILE_PUT,
               DUP, 
-              JUMP_IF, -7,
+              JUMP_IF, -13,
+              GET_GLOBAL, 1,
               PUSH, "Blast off!\n",
-              PRINT
+              FILE_PUT
             ]
           end
         end
@@ -49,16 +62,25 @@ class SimpleBenchmark < Test::Unit::TestCase
     silence do
       result = RubyProf.profile do
         vm.run [
-          PUSH, 11,
+          PUSH, "test.html",
+          PUSH, "w",
+          OPEN_FILE,
+          SET_GLOBAL, 1,
+          PUSH, 11,   
           DECREMENT,
           DUP,
-          PRINT,
+          GET_GLOBAL, 1,
+          SWAP,
+          TO_STRING,
+          FILE_PUT,
+          GET_GLOBAL, 1,
           PUSH, ", ",
-          PRINT,
+          FILE_PUT,
           DUP, 
-          JUMP_IF, -7,
+          JUMP_IF, -13,
+          GET_GLOBAL, 1,
           PUSH, "Blast off!\n",
-          PRINT
+          FILE_PUT
         ]
       end
       file = File.new(File.join(File.dirname(__FILE__), "koi_profile.html"), "w+")
