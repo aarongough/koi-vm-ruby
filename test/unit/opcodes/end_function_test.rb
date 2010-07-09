57bf3d74d8522e9384474279d69d221490f204c1
Aaron Gough <aaron@aarongough.com>
2010-07-09 14:45:55 -0400

    Added test and code for END_FUNCTION opcode

---
+++ test/unit/opcodes/end_function_test.rb
@@ -0,0 +1,14 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', "..", 'test_helper.rb'))
+
+class EndFunctionTest < Test::Unit::TestCase
+
+  include Koi
+  
+  test "END_FUNCTION should do nothing" do
+    state = VM.new.run [
+      END_FUNCTION
+    ]
+    assert_equal 1, state[:fibers][0][:instruction_pointer]
+  end
+  
+end
