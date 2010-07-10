5592443fd391ee19fdf1aafc5dd19365f53e4839
Aaron Gough <aaron@aarongough.com>
2010-07-09 20:52:00 -0400

    Added ARRAY_GET_INDEX opcode

--- lib/koi/opcodes/_opcode_constants.rb
+++ lib/koi/opcodes/_opcode_constants.rb
@@ -1,55 +1,56 @@
 module Koi
-  NO_OP         = 0
-  
-  # 20
-  PUSH          = 1
-  POP           = 2
-  STKSIZE       = 3
-  DUP           = 12
-  SWAP          = 14
-  TOP           = 15
-  
-  # 40
-  ABS_JUMP      = 28 #
-  REL_JUMP      = 5  #
-  JUMP_IF       = 6
-  JUMP_UNLESS   = 7
-  
-  # 60
-  ADD           = 8
-  SUBTRACT      = 9
-  MULTIPLY      = 10
-  DIVIDE        = 11
-  INCREMENT     = 20
-  DECREMENT     = 21
-  
-  # 80
-  SET_LOCAL     = 16 #
-  GET_LOCAL     = 17 #
-  SET_GLOBAL    = 18
-  GET_GLOBAL    = 19
-  
-  # 100
-  FIBER_ID      = 22
-  FIBER_NEW     = 23
-  FIBER_DELETE  = 24
-  
-  # 120
-  FILE_OPEN     = 25
-  FILE_PUT      = 26
-
-  # 140
-  PUSH_FUNCTION = 29 #
-  END_FUNCTION  = 30 #
-  CALL          = 31 #
-  RETURN        = 32 #
-  
-  # 160
-  ARRAY_NEW     = 160
+  NO_OP             = 0
+                    
+  # 20              
+  PUSH              = 1
+  POP               = 2
+  STKSIZE           = 3
+  DUP               = 12
+  SWAP              = 14
+  TOP               = 15
+                    
+  # 40              
+  ABS_JUMP          = 28 #
+  REL_JUMP          = 5  #
+  JUMP_IF           = 6
+  JUMP_UNLESS       = 7
+                    
+  # 60              
+  ADD               = 8
+  SUBTRACT          = 9
+  MULTIPLY          = 10
+  DIVIDE            = 11
+  INCREMENT         = 20
+  DECREMENT         = 21
+                    
+  # 80              
+  SET_LOCAL         = 16 #
+  GET_LOCAL         = 17 #
+  SET_GLOBAL        = 18
+  GET_GLOBAL        = 19
+                    
+  # 100             
+  FIBER_ID          = 22
+  FIBER_NEW         = 23
+  FIBER_DELETE      = 24
+                    
+  # 120             
+  FILE_OPEN         = 25
+  FILE_PUT          = 26
+                    
+  # 140             
+  PUSH_FUNCTION     = 29 #
+  END_FUNCTION      = 30 #
+  CALL              = 31 #
+  RETURN            = 32 #
+                    
+  # 160             
+  ARRAY_NEW         = 160
+  ARRAY_GET_INDEX   = 161
   
   # 180
-  TO_STRING     = 27
+  TO_STRING         = 27
   
   
-  EXIT          = 255
+  EXIT              = 255
 end             
