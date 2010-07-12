9bc5787f82c98b62690b3c1ceb4993a381eb95e4
Aaron Gough <aaron@aarongough.com>
2010-07-11 21:32:48 -0400

    Fixed bug in HASH_GET_KEY

--- lib/koi/opcodes/hash_operations/hash_get_key.rb
+++ lib/koi/opcodes/hash_operations/hash_get_key.rb
@@ -3,6 +3,7 @@ module Koi
     
     # HASH_GET_KEY
     def opcode181
+      expect_stack_size(3)
       fiber = this_fiber
       raise StackError, "Expected array at stack[-2]" unless(fiber[:stack][-2].is_a?(Hash))
       fiber[:stack].push fiber[:stack][-2][fiber[:stack].pop]
