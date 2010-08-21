dd411ce5600e09feac7a9a35a5711958e3c35b6c
Aaron Gough <aaron@aarongough.com>
2010-08-21 16:12:27 -0400

    Implemented PUSH_INT.

---
+++ test/unit/opcodes/push_operations/push_int_test.rb
@@ -0,0 +1,27 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'test_helper.rb'))
+
+class PushIntTest < Test::Unit::TestCase
+
+  include Koi
+  
+  test "should put integer onto stack" do
+    vm = VM.new
+    vm.run [
+      PUSH_INT, 99
+    ]
+    assert_equal [[INTEGER_, 99]], vm.data_stack
+    assert_equal 2, vm.instruction_pointer
+  end
+  
+  test "should raise error if operand is not an integer" do
+    [false, 2.0, "string", [1], {:t=>1}].each do |x|
+      assert_raises OperandError do
+        vm = VM.new
+        vm.run [
+          PUSH_INT, x
+        ]
+      end
+    end
+  end
+  
+end
