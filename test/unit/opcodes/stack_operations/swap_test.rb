1c9b836573c56bb21bc12ac4f623e700ede08c9e
Aaron Gough <aaron@aarongough.com>
2010-08-21 17:50:08 -0400

    Implemented SWAP.

---
+++ test/unit/opcodes/stack_operations/swap_test.rb
@@ -0,0 +1,36 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'test_helper.rb'))
+
+class SwapTest < Test::Unit::TestCase
+
+  include Koi
+  
+  test "should swap the two topmost items on the data stack" do
+    vm = VM.new
+    vm.data_stack = [1,2]
+    vm.run [
+      SWAP
+    ]
+    assert_equal [2,1], vm.data_stack
+    assert_equal 1, vm.instruction_pointer
+  end
+  
+  test "should raise StackError if there are no items on the stack" do
+    assert_raises StackError do
+      vm = VM.new
+      vm.run [
+        SWAP
+      ]
+    end
+  end
+  
+  test "should raise StackError if there are less than 2 items on the stack" do
+    assert_raises StackError do
+      vm = VM.new
+      vm.data_stack = [1]
+      vm.run [
+        SWAP
+      ]
+    end
+  end
+  
+end
