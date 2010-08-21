f5cee3559d68d2e6de6eef6d8e6692aa2c1d9d46
Aaron Gough <aaron@aarongough.com>
2010-08-21 16:18:35 -0400

    Implemented PUSH_STRING.

---
+++ test/unit/opcodes/push_operations/push_string_test.rb
@@ -0,0 +1,27 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'test_helper.rb'))
+
+class PushStringTest < Test::Unit::TestCase
+
+  include Koi
+  
+  test "should push string onto stack" do
+    vm = VM.new
+    vm.run [
+      PUSH_STRING, "test"
+    ]
+    assert_equal [[STRING_, "test"]], vm.data_stack
+    assert_equal 2, vm.instruction_pointer
+  end
+  
+  test "should raise error if operand is not a string" do
+    [false, 2, 2.0, [1], {:t=>1}].each do |x|
+      assert_raises OperandError do
+        vm = VM.new
+        vm.run [
+          PUSH_STRING, x
+        ]
+      end
+    end
+  end
+  
+end
