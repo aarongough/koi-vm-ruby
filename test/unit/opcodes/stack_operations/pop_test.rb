fdaaeedaf280be5513e1e58421495c15bdd851ed
Aaron Gough <aaron@aarongough.com>
2010-08-21 17:22:45 -0400

    Implemented POP.

---
+++ test/unit/opcodes/stack_operations/pop_test.rb
@@ -0,0 +1,28 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'test_helper.rb'))
+
+class PopTest < Test::Unit::TestCase
+
+  include Koi
+  
+  test "should pop a single item off the data stack" do
+    (1..10).each do |x|
+      vm = VM.new
+      vm.data_stack.fill("x", 0..(x - 1))
+      assert_equal x, vm.data_stack.length
+      vm.run [
+        POP
+      ]
+      assert_equal x - 1, vm.data_stack.length
+    end
+  end
+  
+  test "should raise StackError if there are no items on the stack" do
+    assert_raises StackError do
+      vm = VM.new
+      vm.run [
+        POP
+      ]
+    end
+  end
+  
+end
