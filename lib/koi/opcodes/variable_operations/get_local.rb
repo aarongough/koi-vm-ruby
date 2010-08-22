cfb486d6d0bdc7652d2d103661fec1797e2dc23a
Aaron Gough <aaron@aarongough.com>
2010-08-22 19:17:07 -0400

    Implemented GET_LOCAL.

---
+++ lib/koi/opcodes/variable_operations/get_local.rb
@@ -0,0 +1,10 @@
+module Koi
+  class VM
+    
+    @@instruction[GET_LOCAL] = Proc.new() do |vm|
+      vm.data_stack.push( vm.locals[vm.opcodes[vm.instruction_pointer + 1]] )
+      vm.instruction_pointer = vm.instruction_pointer + 2
+    end
+    
+  end
+end
