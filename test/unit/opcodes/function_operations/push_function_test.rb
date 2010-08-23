b51864c0247906b7b6d761588fe1358471f93884
Aaron Gough <aaron@aarongough.com>
2010-08-22 20:13:35 -0400

    Implemented PUSH_FUNCTION and END_FUNCTION.

---
+++ test/unit/opcodes/function_operations/push_function_test.rb
@@ -0,0 +1,21 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'test_helper.rb'))
+
+class PushFunctionTest < Test::Unit::TestCase
+
+  include Koi
+  
+  test "should push function reference onto stack" do
+    vm = VM.new
+    vm.run [
+      PUSH_FUNCTION, 1,
+      NO_OP,
+      NO_OP,
+      END_FUNCTION, 0,
+      END_FUNCTION, 1
+    ]
+    assert_equal [[FUNCTION_, 2]], vm.data_stack
+    assert_equal 8, vm.instruction_pointer
+  end
+
+  
+end
