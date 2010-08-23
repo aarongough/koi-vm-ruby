a4c5eb4e32a50b101464ad0152947783be69d141
Aaron Gough <aaron@aarongough.com>
2010-08-22 21:13:04 -0400

    Updated README, examples and simple_benchmark.rb

--- examples/blastoff.rb
+++ examples/blastoff.rb
@@ -3,22 +3,18 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'koi.rb'
 include Koi
 
 VM.new.run [
-  PUSH, 1,    # Open STDOUT for writing
-  PUSH, "w",
-  FILE_OPEN,
-  PUSH, 11,   
-  DECREMENT,
+  PUSH_INT, 11,
+  PUSH_INT, 1,
+  SUBTRACT,
   DUP,
-  PUSH, 1,
-  SWAP,
   TO_STRING,
-  FILE_PUT,
-  PUSH, 1,
-  PUSH, ", ",
-  FILE_PUT,
-  DUP, 
-  JUMP_IF, -13,
-  PUSH, 1,
-  PUSH, "Blast off!\n",
-  FILE_PUT
+  PRINT,
+  PUSH_STRING, ", ",
+  PRINT,
+  DUP,
+  PUSH_INT, 0,
+  EQUAL,
+  JUMP_UNLESS, -13,
+  PUSH_STRING, "Blast Off!\n",
+  PRINT
 ]
--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -3,9 +3,6 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'koi.rb'
 include Koi
 
 VM.new.run [
-  PUSH, 1,
-  PUSH, "w",
-  FILE_OPEN,
-  PUSH, "Hello World!\n",
-  FILE_PUT
+  PUSH_STRING, "Hello World!\n",
+  PRINT
 ]
