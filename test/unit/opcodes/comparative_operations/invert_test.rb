f6efe67537d06abd4a3257727afe7fe94f8457e9
Aaron Gough <aaron@aarongough.com>
2010-08-25 16:53:28 -0400

    Implemented INVERT.

---
+++ test/unit/opcodes/comparative_operations/invert_test.rb
@@ -0,0 +1,44 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'test_helper.rb'))
+
+class InvertTest < Test::Unit::TestCase
+
+  include KoiVM
+  
+  test "should invert boolean value true -> false" do
+    vm = VM.new
+    vm.data_stack = [[BOOL_, true]]
+    vm.run [
+      INVERT
+    ]
+    assert_equal [[BOOL_, false]], vm.data_stack
+  end
+  
+  test "should invert boolean value false -> true" do
+    vm = VM.new
+    vm.data_stack = [[BOOL_, false]]
+    vm.run [
+      INVERT
+    ]
+    assert_equal [[BOOL_, true]], vm.data_stack
+  end
+  
+  test "should raise StackError topmost stack value is not a bool" do
+    assert_raises StackError do
+      vm = VM.new
+      vm.data_stack = [[INTEGER_, 1]]
+      vm.run [
+        INVERT
+      ]
+    end
+  end
+  
+  test "should raise StackError if there are no items on the stack" do
+    assert_raises StackError do
+      vm = VM.new
+      vm.run [
+        INVERT
+      ]
+    end
+  end
+  
+end
