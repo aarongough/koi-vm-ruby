b6d507c6726f3ca3bd84b8f82a6d5e669b22c7b5
Aaron Gough <aaron@aarongough.com>
2010-08-27 13:32:58 -0400

    Fixed bug in PUSH_FUNCTION when function definitions were nested.

--- test/unit/opcodes/function_operations/push_function_test.rb
+++ test/unit/opcodes/function_operations/push_function_test.rb
@@ -10,12 +10,32 @@ class PushFunctionTest < Test::Unit::TestCase
       PUSH_FUNCTION, 1,
       NO_OP,
       NO_OP,
+      RETURN,
       END_FUNCTION, 0,
       END_FUNCTION, 1
     ]
     assert_equal [[FUNCTION_, 2]], vm.data_stack
-    assert_equal 8, vm.instruction_pointer
+    assert_equal 9, vm.instruction_pointer
   end
-
   
+  test "should push function reference onto stack that contains nested function definition" do
+    vm = VM.new
+    vm.run [
+      PUSH_FUNCTION, 6,
+      NO_OP,
+        PUSH_FUNCTION, 2,
+        NO_OP,
+        RETURN,
+        END_FUNCTION, 0,
+        END_FUNCTION, 2,
+      NO_OP,
+      RETURN,
+      END_FUNCTION, 0,
+      END_FUNCTION, 6
+    ]
+    assert_equal [[FUNCTION_, 2]], vm.data_stack
+    assert_equal 17, vm.instruction_pointer
+  end
+  
+    
 end
