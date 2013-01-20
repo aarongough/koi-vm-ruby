6ccb7396959cd7d2d9197522c278c06fda1e840d
Patrick Jones <patrick@patrickjones.ca>
2013-01-19 23:34:29 -0500

    changed the order in which the required files are loaded

--- lib/koi-vm-ruby.rb
+++ lib/koi-vm-ruby.rb
@@ -3,7 +3,7 @@ require_files = []
 require_files << File.join(File.dirname(__FILE__), "koi-vm-ruby", "core.rb")
 require_files << File.join(File.dirname(__FILE__), "koi-vm-ruby", "helpers.rb")
 require_files << File.join(File.dirname(__FILE__), "koi-vm-ruby", "exceptions.rb")
-require_files.concat Dir[File.join(File.dirname(__FILE__), 'koi-vm-ruby', '**', '*.rb')]
+require_files.concat Dir[File.join(File.dirname(__FILE__), 'koi-vm-ruby', '**', '*.rb')].sort
 
 require_files.each do |file|
   require File.expand_path(file)
